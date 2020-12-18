6a4326afd588153203652a5e67ed990f86304fc5
Tionis Nagir <tionis@tasadar.net>
2020-12-18 17:05:40 +0100

    build: fixed docker compose files

--- scripts/start-local-db.sh
+++ scripts/start-local-db.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 docker run -it --rm -e POSTGRES_PASSWORD=api-password -e POSTGRES_USER=api -e POSTGRES_DB=api -p 5432:5432 postgres:latest
+#docker run --name dev-postgres -e POSTGRES_PASSWORD=password -e POSTGRES_USER=test-user -e POSTGRES_DB -p 5432:5432 -d postgres
